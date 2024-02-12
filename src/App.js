5d4346c0313f51eab7826981e5e55968ecba4419
mythreyeepn <mythreyeepn@gmail.com>
2024-02-12 17:08:39 -0600

    changges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React ,  {useRef} from "react";
 import "./style.css";
+export function useIsFirstRender() {
+  // your code here
+  const isFirstRender = useRef(true)
+
+  if(isFirstRender.current){
+    isFirstRender.current=false
+    return true;
+  }
+return false;
+}
+
+// if you want to try your code on the right panel
+// remember to export App() component like below
 
 export default function App() {
+    const isFirstRender = useIsFirstRender()
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {isFirstRender ? <p>First render</p> : <p>Not first render</p>}
+
     </div>
   );
 }
